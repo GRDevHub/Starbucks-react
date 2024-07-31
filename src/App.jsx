6930ee170a7a965442f983fa0a631e376525670f
Gabriel Riveros <velo02.gr@gmail.com>
2024-07-31 19:41:02 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+import Header from "./assets/Components/Header"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App;
---
+++ src/assets/Components/Nav.jsx
@@ -0,0 +1,7 @@
+function Nav() {
+    return ( 
+        <nav className='container nav'></nav>
+    );
+}
+
+export default Nav;
